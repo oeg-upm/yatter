86b280737d8c1523968ed235fa5aeaaf344f5ef4
LuisLopezPI <luis.lp99@gmail.com>
2021-05-17 21:36:38 +0200

    default prefixes added

--- lib/mapping.py
+++ lib/mapping.py
@@ -1,5 +1,4 @@
 import yaml
-
 def addMapping(data, mapping,it):
     map_template="<#"+mapping+"_"+str(it)+"> a rr:TriplesMap;\n\n"
     return map_template
@@ -10,6 +9,12 @@ def addPrefix(data):
         prefixes=data.get("prefixes")
         for prefix in prefixes:
             template+="@prefix "+ prefix +": <"+data.get("prefixes").get(prefix)+">.\n"
+        if "rr" not in data.get("prefixes"):
+            template+="@prefix rr: <http://www.w3.org/ns/r2rml#>."
+        if "rml" not in data.get("prefixes"):
+            template+="@prefix rml: <http://semweb.mmlab.be/ns/rml#>."
+        if "rdf" not in data.get("prefixes"):
+            template+="@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>."
         template+="\n\n"
         return template
     else:
