ec989dad25021800d746c0fb1af7c5ec16a41f62
LuisLopezPi <58739138+LuisLopezPi@users.noreply.github.com>
2021-06-10 09:29:48 +0200

    Delete prueba.py

--- lib/prueba.py
+++
@@ -1,9 +0,0 @@
-import rdflib
-
-g = rdflib.Graph()
-#result = g.parse('trees.ttl')
-#result = g.parse('trees.ttl', format='ttl')
-result = g.parse('prueba.rml.ttl', format='n3')
-print (len(g))
-for stmt in g:
-    print (stmt)
