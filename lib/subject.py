7282f50a4a2fd7450e7c856e689ab3ae1a31b6e2
LuisLopezPi <58739138+LuisLopezPi@users.noreply.github.com>
2021-06-10 09:32:48 +0200

    Update subject.py

--- lib/subject.py
+++ lib/subject.py
@@ -1,73 +1,115 @@
 import yaml
 
 def addSubject(data, mapping):
-    subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
     list_subject=[]
-    list_subject.append("s")
-    final_list =[]
-    if ("s" in data.get("mappings").get(mapping) ):
-        list_subject.append(data.get("mappings").get(mapping).get("s"))
-        if(len(list_subject[1][0])==1):
-            #un solo sujeto
-            subject = "".join(data.get("mappings").get(mapping).get("s"))
-            termMap=getTermMap(subject)
-            subject=subject.replace("$(","{").replace(")","}")
-            subject_template = subject_template + termMap+' "'  + subject +'"'+ "\n\t];"+"\n\n"
-            final_list.append(subject_template)
-            return final_list
-        else:
-            #varios sujetos
-            for x in list_subject[1]:
+    final_list=[]
+
+    if ("s" in data.get("mappings").get(mapping)):
+        list_subject=data.get("mappings").get(mapping).get("s")
+        if (type(list_subject)== list): #varios sujetos
+            for subject in list_subject:
                 subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
-                termMap=getTermMap(x)
-                x = x.replace("$(","{").replace(")","}")
-                subject_template = subject_template + termMap+' "'  + x +'"'+ "\n\t];"+"\n\n"
+                termMap=getTermMap(subject)
+                if(termMap=='rr:template "'):
+                    subject=subject.replace("$",'')
+                    subject=subject.replace("(","{")
+                    subject=subject.replace(")",'}')
+                else:
+                    subject=subject.replace("$(",'')
+                    subject=subject.replace(")",'')
+                if(termMap!='rr:constant '):
+                    subject_template+= termMap + ' "'+ subject + '"'+ "\n\t];\n\n"
+                else:
+                    subject_template+= termMap + subject + "\n\t];\n\n"
                 final_list.append(subject_template)
-            return final_list
+
+
+        else: #un solo sujeto
+            subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
+            subject=list_subject
+            termMap=getTermMap(subject)
+            if(termMap=='rr:template "'):
+                subject=subject.replace("$",'')
+                subject=subject.replace("(","{")
+                subject=subject.replace(")",'}')
+            else:
+                subject=subject.replace("$(",'')
+                subject=subject.replace(")",'')
+            if(termMap!='rr:constant '):
+                subject_template+= termMap + ' "'+ subject + '"'+ "\n\t];\n\n"
+            else:
+                subject_template+= termMap + subject + "\n\t];\n\n"
+            final_list.append(subject_template)
+
+
+    elif ("subjects" in data.get("mappings").get(mapping)):
+        list_subject= data.get("mappings").get(mapping).get("subjects")
+        if (type(list_subject)== list): #varios sujetos
+            for subject in list_subject:
+                subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
+                termMap=getTermMap(subject)
+                if(termMap=='rr:template "'):
+                    subject=subject.replace("$",'')
+                    subject=subject.replace("(","{")
+                    subject=subject.replace(")",'}')
+                else:
+                    subject=subject.replace("$(",'')
+                    subject=subject.replace(")",'')
+                if(termMap!='rr:constant '):
+                    subject_template+= termMap + ' "'+ subject + '"'+ "\n\t];\n\n"
+                else:
+                    subject_template+= termMap + subject + "\n\t];\n\n"
+                final_list.append(subject_template)
+
+
+        else: #un solo sujeto
+            subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
+            subject=list_subject
+            termMap=getTermMap(subject)
+            if(termMap=='rr:template "'):
+                subject=subject.replace("$",'')
+                subject=subject.replace("(","{")
+                subject=subject.replace(")",'}')
+            else:
+                subject=subject.replace("$(",'')
+                subject=subject.replace(")",'')
+            if(termMap!='rr:constant '):
+                subject_template+= termMap + ' "'+ subject + '"'+ "\n\t];\n\n"
+            else:
+                subject_template+= termMap + subject + "\n\t];\n\n"
+            final_list.append(subject_template)
+
+
+    elif ("subject" in data.get("mappings").get(mapping)):
+        list_subject=data.get("mappings").get(mapping).get("subject")
+        if (type(list_subject)== list): #varios sujetos ERROR
+            raise Exception("ERROR: Only one subject should be in mapping: " + mapping)
+
+        else: #un solo sujeto
+            subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
+            subject=list_subject
+            termMap=getTermMap(subject)
+            if(termMap=='rr:template "'):
+                subject=subject.replace("$",'')
+                subject=subject.replace("(","{")
+                subject=subject.replace(")",'}')
+            else:
+                subject=subject.replace("$(",'')
+                subject=subject.replace(")",'')
+            if(termMap!='rr:constant '):
+                subject_template+= termMap + ' "'+ subject + '"'+ "\n\t];\n\n"
+            else:
+                subject_template+= termMap + subject + "\n\t];\n\n"
+            final_list.append(subject_template)
+
 
     else:
-        if("subjects" in data.get("mappings").get(mapping)):
-            list_subject.append(data.get("mappings").get(mapping).get("subjects"))
-            if(len(list_subject[1][0])==1):
-                #un solo sujeto
-                subject = "".join(data.get("mappings").get(mapping).get("subjects"))
-                termMap=getTermMap(subject)
-                subject=subject.replace("$(","{").replace(")","}")
-                subject_template = subject_template+ termMap + ' "'  + subject +'"'+ "\n\t];"+"\n\n"
-                final_list.append(subject_template)
-                return final_list
-            else:
-                print("si")
-                #varios sujetos
-                for x in list_subject[1]:
-                    termMap=getTermMap(x)
-                    subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
-                    x = x.replace("$(","{").replace(")","}")
-                    subject_template = subject_template +termMap+ ' "'  + x +'"'+ "\n\t];"+"\n\n"
-                    final_list.append(subject_template)
-                return final_list
+        raise Exception("ERROR: no subjects in mapping " + mapping)
 
-        elif ("subject" in data.get("mappings").get(mapping)):
-            list_subject.append(data.get("mappings").get(mapping).get("subject"))
-            if(len(list_subject[1][0])==1):
-                #un solo sujeto
-                subject = "".join(data.get("mappings").get(mapping).get("subject"))
-                termMap=getTermMap(subject)
-                subject=subject.replace("$(","{").replace(")","}")
-                subject_template = subject_template+termMap + ' "'  + subject +'"'+ "\n\t];"+"\n\n"
-                final_list.append(subject_template)
-                return final_list
-            else:
-                #varios sujetos
-                for x in list_subject[1]:
-                    termMap=getTermMap(subject)
-                    subject_template = "\trr:subjectMap [\n" + "\t\ta rr:SubjectMap;\n"+ "\t\t"
-                    x = x.replace("$(","{").replace(")","}")
-                    subject_template = subject_template+termMap + ' "'  + x +'"'+ "\n\t];"+"\n\n"
-                    final_list.append(subject_template)
-                return final_list
-        else:
-            raise Exception("ERROR: no subjects in mapping " + mapping)
+
+    return final_list
+
+
 
 def getTermMap(text):
     if("$(" in text and ")" in text):
