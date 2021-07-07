a4be3b4981751b659eb403ed4cbaccac6d795327
David Chaves <dachafra@gmail.com>
2021-07-07 13:27:31 +0200

    refactoring source generation

--- lib/source.py
+++ lib/source.py
@@ -10,156 +10,139 @@ def get_initial_sources(data):
 
 
 def add_source(data, mapping, list_initial_sources):
+    source_template = "\t" + constants.RML_LOGICAL_SOURCE + " [\n\t\ta " + constants.RML_LOGICAL_SOURCE_PROPERTY + \
+                      ";\n\t\t" + constants.RML_SOURCE + " "
     final_list = []
 
     if constants.YARRRML_SOURCES in data.get(constants.YARRRML_MAPPINGS).get(mapping):
         sources = data.get(constants.YARRRML_MAPPINGS).get(mapping).get(constants.YARRRML_SOURCES)
-    elif constants.YARRRML_SOURCE in data.get(constants.YARRRML_MAPPINGS).get(mapping):
+    elif constants.YARRRML_SOURCE in data.get(constants.YARRRML_MAPPINGS).get(mapping):  # don't find this exist
         sources = data.get(constants.YARRRML_MAPPINGS).get(mapping).get(constants.YARRRML_SOURCE)
     else:
         raise Exception("ERROR: sources not defined in mapping " + mapping)
 
-    if type(sources) is str:  # una unica source
-        final_list.append(add_initial_source(data, sources, mapping))
-    else:
-        for source in sources:  # varias sources
-            if source in list_initial_sources:
-                final_list.append(add_initial_source(data, source, mapping))
-            elif "access" in source:
-                final_list.append(addSourceFull(data, mapping, source))
-            elif type(source) is list: #cuidado con esto
-                final_list.append(addSourceSimplified(data, mapping, source))
+    for source in sources:
+        if source in list_initial_sources:
+            source = data.get(constants.YARRRML_SOURCES).get(source)
+
+        if constants.YARRRML_ACCESS in source:
+            if constants.YARRRML_QUERY in source:
+                final_list.append(database_source(mapping, source))
             else:
-                raise Exception("ERROR: source " + source + " in mapping " + mapping + " not valid")
+                final_list.append(source_template + add_source_full(mapping, source))
+        elif type(source) is list:
+            final_list.append(source_template + add_source_simplified(mapping, source))
+        else:
+            raise Exception("ERROR: source " + source + " in mapping " + mapping + " not valid")
     return final_list
 
 
-def add_initial_source(data, sources, mapping):
-    if type(data.get("sources").get(sources)) is list:
-        return addSourceSimplified(data, mapping, data.get("sources").get(sources))
-    else:
-        return addSourceFull(data, mapping, data.get("sources").get(sources))
+def add_source_simplified(mapping, source):
+    source_rdf = ""
+    file_path = source[0]
+    source_reference_formulation = source.split('~')[1]
+    source_extension = file_path.split('.')[1]
 
-
-def addSourceSimplified(data, mapping, sources):
-    source_template = "\trml:logicalSource [\n" + "\t\ta rml:logicalSource;\n" + "\t\trml:source "
-    source = sources[0]
-    source_aux = source.split('~')
-    source_aux1 = source_aux[0].split('.')
-    source_name = source_aux1[0]
-    source_extension = source_aux1[1]
-    source_referenceFormulation = source_aux[1]
-    if (checkExtension(source_extension, source_referenceFormulation) == 1):
+    if switch_in_reference_formulation(source_reference_formulation) != source_extension:
         raise Exception(
-            "ERROR: mismatch extension and referenceFormulation in source " + sources + " in mapping " + mapping)
+            "ERROR: mismatch extension and referenceFormulation in source " + source + " in mapping " + mapping)
     else:
-        if len(sources) == 1:
-            if (source_extension == "csv" or source_extension == "SQL2008"):
-                source_template += '"' + source_aux[
-                    0] + '"' + ";\n" + "\t\trml:referenceFormulation ql:" + source_referenceFormulation + "\n" + "\t];\n"
+        if len(source) == 1:  # si no tiene iterador
+            if source_extension == "csv" or source_extension == "SQL2008":
+                source_rdf += '"' + file_path + '"' + ";\n" + "\t\t" + constants.RML_REFERENCE_FORMULATION + " ql:" \
+                              + source_reference_formulation + "\n" + "\t];\n"
             else:
-                raise Exception("ERROR: source " + sources + " in mapping " + mapping + " has no iterator")
+                raise Exception("ERROR: source " + source + " in mapping " + mapping + " has no iterator")
+        else:  # source[1] es el iterador en json y xml
+            source_rdf += "\"" + file_path + "\";\n\t\t" + constants.RML_REFERENCE_FORMULATION + " ql:" \
+                          + source_reference_formulation + ";\n\t\t" + constants.RML_ITERATOR + "\"" \
+                          + source[1] + "\";\n\t];\n"
+    return source_rdf
+
+
+def add_source_full(mapping, source):
+    source_rdf = ""
+
+    access = str(source.get(constants.YARRRML_ACCESS))
+    extension = source.split(".")[1]
+
+    if constants.YARRRML_REFERENCE_FORMULATION in source:
+        reference_formulation = str(source.get(constants.YARRRML_REFERENCE_FORMULATION))
+        format_from_reference = switch_in_reference_formulation(reference_formulation)
+        if extension == format_from_reference or format_from_reference == "ERROR":
+            raise Exception("ERROR: not referenceFormulation found or mismatch between the format and "
+                            "referenceFormulation in source " + access + "in mapping " + mapping)
+
+        if constants.YARRRML_ITERATOR in source:
+            source_iterator = str(source.get(constants.YARRRML_ITERATOR))
+            source_rdf += "\"" + access + "\";\n\t\t" + constants.RML_REFERENCE_FORMULATION + " ql:" \
+                          + reference_formulation + ";\n\t\t" + constants.RML_ITERATOR + "\"" \
+                          + source_iterator + "\"\n\t];\n"
         else:
-            source_delim = sources[1]
-            source_template += '"' + source_aux[
-                0] + '"' + ";\n" + "\t\trml:referenceFormulation ql:" + source_referenceFormulation + ";\n" + '\t\trml:iterator "' + source_delim + '"' + ";\n\t];\n"
-    return source_template
-
-
-def addSourceFull(data, mapping, sour):
-    source_final = ""
-    if "query" in sour:
-        list_source = databaseSource(data, mapping, sour)
-        return list_source
-    list_sources = []
-    list_sources.append("sources")
-    list_sources.append(data.get("mappings").get(mapping).get("sources"))
-    source_template = "\trml:logicalSource [\n" + "\t\ta rml:logicalSource;\n" + "\t\trml:source "
-    if "access" in sour:
-        source = str(sour.get("access"))
-        extension = source.split(".")
-    else:
-        raise Exception('ERROR: source' + sour.get("access") + 'in mapping ' + mapping + ' has no "access"')
-
-    if "referenceFormulation" in sour:
-        source_referenceFormulation = str(sour.get("referenceFormulation"))
-        if checkExtension(extension[1], source_referenceFormulation) == 1:
-            raise Exception("ERROR: mismatch extension and referenceFormulation in source " + sour.get(
-                "access") + "in mapping " + mapping)
-        if getReferenceFOrmulation(source_referenceFormulation) != "ERROR":
-            source_referenceFormulation = getReferenceFOrmulation(source_referenceFormulation)
-    else:
-        if extension[1] == "csv":
-            if "iterator" in sour:
-                source_iterator = str(sour.get("iterator"))
-                source_template += '"' + source + '"' + ";\n" + '\t\trml:iterator "' + source_iterator + '"\n\t];\n'
-                return source_template
+            if extension == "csv" or extension == "SQL2008":
+                source_rdf += "\"" + access + "\";\n\t\t" + constants.RML_REFERENCE_FORMULATION + " ql:" \
+                              + reference_formulation + ";\n\n\t];\n"
             else:
-                source_template += '"' + source + '"' + ";\n" + '"\n\t];\n'
-                return source_template
-        else:
-            raise Exception(
-                "ERROR: source " + sour.get("access") + "in mapping " + mapping + " has no referenceFormulation")
-    if "iterator" in sour:
-        source_iterator = str(sour.get("iterator"))
-        source_template += '"' + source + '"' + ";\n" + "\t\trml:referenceFormulation ql:" + source_referenceFormulation + ";\n" + '\t\trml:iterator "' + source_iterator + '"\n\t];\n'
+                raise Exception("ERROR: source " + access + "in mapping " + mapping + " has no referenceFormulation")
+
     else:
-        if extension[1] == "csv" or extension[1] == "SQL2008":
-            source_template += '"' + source + '"' + ";\n" + "\t\trml:referenceFormulation ql:" + source_referenceFormulation + ";\n" + '"\n\t];\n'
+        if extension == "csv":
+            source_rdf += "\"" + access + "\";\n\n\t];\n"
         else:
-            raise Exception("ERROR: source " + sour.get("access") + " in mapping " + mapping + " has no iterator")
-    source_final += source_template
-    return source_final
+            raise Exception("ERROR: source " + access + "in mapping " + mapping + " has no referenceFormulation")
+
+    return source_rdf
 
 
-def checkExtension(extension, referenceFormulation):
-    switcher = {
-        "json": "jsonpath",
-        "csv": "csv",
-        "xml": "xpath"
-        # para querys
-    }
-    reference = switcher.get(extension, "ERROR")
-    if reference == referenceFormulation:
-        return 0
-    else:
-        return 1
-
-
-def getReferenceFOrmulation(referenceFormulation):
-    switcher = {
-        "jsonpath": "JSONPath",
-        "csv": "CSV",
-        "xpath": "XPath"
-    }
-    return switcher.get(referenceFormulation, "ERROR")
-
-
-def databaseSource(data, mapping, source):
-    list = []
-    templatelog = "\trml:logicalSource [\n\t\ta rml:LogicalSource;\n"
-    if "access" in source:
-        if "credentials" in source:
-            if "type" in source and source.get("type") == "mysql":
-                templatelog += '\t\trml:source <#DataSource_' + mapping + '>;\n\t\trr:sqlVersion rr:SQL2008;\n\t\trml:query "' + source.get(
-                    "query") + '";\n\t\t'
-                if "referenceFormulation" in source:
-                    formu = getReferenceFOrmulation(source.get("referenceFormulation"))
-                    templatelog += "\t\trml:referenceFormulation ql:" + formu + "\n\t];\n"
+def database_source(mapping, source):
+    source_database = []
+    source_rdf = ""
+    if constants.YARRRML_ACCESS in source:
+        if constants.YARRRML_CREDENTIALS in source:
+            if constants.YARRRML_TYPE in source:
+                source_rdf += "<#DataSource_" + mapping + ">;\n\t\t" \
+                              + constants.R2RML_SQL_VERSION + " rr:SQL2008;\n\t\t" \
+                              + constants.R2RML_SQL_QUERY + " \"" + source.get(constants.YARRRML_QUERY) + "\";\n\t\t"
+                if constants.YARRRML_REFERENCE_FORMULATION in source:
+                    source_rdf += "\t\t" + constants.RML_REFERENCE_FORMULATION + " ql:" \
+                                  + switch_in_reference_formulation(
+                                    source.get(constants.YARRRML_REFERENCE_FORMULATION)) + "\n\t];\n"
                 else:
-                    templatelog += "\n\t];\n"
+                    source_rdf += "\n\t];\n"
+                source_database.append(source_rdf)
+                source_database.append(generate_database_connection(mapping, source))
 
         else:
             raise Exception("ERROR: no credentials to get access to source in mapping " + mapping)
     else:
         raise Exception("ERROR: no access to the source in mapping " + mapping)
 
-    list.append(templatelog)
+    return source_database
 
-    templateDat = '<#DataSource_' + mapping + '> a d2rq:Database;\n\tdrr1:jdbcDSN "' + source.get(
-        "access") + '";\n\td2rq:jdbcDriver "com.mysql.jdbc.Driver";\n\td2rq:username "' + source.get("credentials").get(
-        "username") + '";\n\td2rq:password "' + source.get("credentials").get("password") + '".\n\n'
 
-    list.append(templateDat)
+def switch_in_reference_formulation(value):
+    switcher = {
+        "jsonpath": "JSONPath",
+        "csv": "CSV",
+        "xpath": "XPath"
+    }
+    return switcher.get(value, "ERROR")
 
-    return list
+
+def generate_database_connection(mapping, source):
+    type = source.get(constants.YARRRML_TYPE)
+    if type == "mysql":
+        driver = "com.mysql.jdbc.Driver"
+    elif type == "postgresql":
+        driver = "org.postgresql.Driver"
+    elif type == "sqlserver":
+        driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver"
+    else:
+        driver = ""
+
+    database = "<#DataSource_" + mapping + "> a " + constants.D2RQ_DATABASE_CLASS + ";\n\t" \
+               + constants.D2RQ_DSN + " \"" + source.get(constants.YARRRML_ACCESS) + "\";\n\t" \
+               + constants.D2RQ_DRIVER + " \"" + driver + "\";\n\t" \
+               + constants.D2RQ_USER + " \"" + source.get(constants.YARRRML_CREDENTIALS).get(constants.YARRRML_USERNAME) + "\";\n\t" \
+               + constants.D2RQ_PASS + " \"" + source.get(constants.YARRRML_CREDENTIALS).get(constants.YARRRML_PASSWORD) + "\".\n\n"
+    return database
