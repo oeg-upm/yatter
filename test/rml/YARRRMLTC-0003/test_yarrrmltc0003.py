b2ae83e00f594b6ac48d847e9b1aaab69b63f8da
leviheichou <marinogonzalezgarcia@gmail.com>
2022-11-30 01:40:02 +0100

    pytests generated, yarrrml maps corrected

---
+++ test/rml/YARRRMLTC-0003/test_yarrrmltc0003.py
@@ -0,0 +1,25 @@
+__author__ = "Marino Gonzalez Garcia"
+__credits__ = ["Marino Gonzalez Garcia"]
+
+__license__ = "Apache-2.0"
+__maintainer__ = "Marino Gonzalez Garcia"
+__email__ = "marino.gonzalez.garcia@alumnos.upm.es"
+
+
+import os
+import yaml
+import yarrrml_translator
+from rdflib.graph import Graph
+from rdflib import compare
+RML_URI = 'http://semweb.mmlab.be/ns/rml#'
+
+
+def test_yarrrmltc0003():
+    expected_mapping = Graph()
+    expected_mapping.parse(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.rml.ttl'), format="ttl")
+
+    translated_mapping = Graph()
+    mapping_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.yml')
+    translated_mapping.parse(data=yarrrml_translator.translate(yaml.safe_load(open(mapping_path)), mapping_format=RML_URI), format="ttl")
+
+    assert compare.isomorphic(expected_mapping, translated_mapping)
