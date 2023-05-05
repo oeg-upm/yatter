529345980885bb76158242e7c8d7b09f3962da9c
David Chaves <dachafra@gmail.com>
2023-05-05 11:57:45 +0200

    adding tec test from @dgarijo

---
+++ test/projects/TEC/test_tec.py
@@ -0,0 +1,27 @@
+__author__ = "David Chaves-Fraga"
+__credits__ = ["David Chaves-Fraga"]
+
+__license__ = "Apache-2.0"
+__maintainer__ = "David Chaves-Fraga"
+__email__ = "david.chaves@upm.es"
+
+
+
+import os
+from ruamel.yaml import YAML
+import yatter
+from rdflib.graph import Graph
+from rdflib import compare
+RML_URI = 'http://semweb.mmlab.be/ns/rml#'
+
+
+def test_yarrrml_tec():
+    expected_mapping = Graph()
+    expected_mapping.parse(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.ttl'), format="ttl")
+
+    translated_mapping = Graph()
+    yaml = YAML(typ='safe', pure=True)
+    mapping_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'mapping.yml')
+    translated_mapping.parse(data=yatter.translate(yaml.load(open(mapping_path)), mapping_format=RML_URI), format="ttl")
+
+    assert compare.isomorphic(expected_mapping, translated_mapping)
